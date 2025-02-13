6e6526fd6c309f464056b2c52b2fb3e5b94672f1
Ruben <ruben13478@gmail.com>
2025-02-12 22:00:56 -0600

    assignemnet added for the next one

--- Loops&Conditionals/secret-number-game.js
+++ Loops&Conditionals/secret-number-game.js
@@ -4,16 +4,17 @@ var point = 0;
 var message = "Sorry, Better Luck Next Time";
 var guess = prompt("Enter your favorite number:");
 guess = parseInt(guess);
-//forget until you get a number
+//loop until you get a number
 while(isNaN(guess)) {
     guess = prompt("that is not a number, please enter a number.");
     guess = parseInt(guess);
 }
+//once you get a numbers
 if(secret_number==guess){
     point += 10;
     message = "Congratulations You Guessed The Secret Number!";
 }
-
+//to load after the prompt
 window.onload = function() {
     document.getElementById("points").innerText = point;
     document.getElementById("message").innerText = message;
